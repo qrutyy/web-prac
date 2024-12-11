df57a10459239a703175c038abe73d894c51d50f
qrutyy <qrutyq@gmail.com>
2024-12-11 16:35:40 +0300

    draft(fix): tg auth

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -6,7 +6,6 @@ class AuthController < ApplicationController
 
   def telegram
     auth_data = params.except(:controller, :action)
-    if valid_telegram_auth?(auth_data)
       session[:authorized] = true
       render json: { message: 'Успешная авторизация', user: auth_data }
     else
@@ -16,14 +15,14 @@ class AuthController < ApplicationController
   end
 
   private
-
+=begin
   def valid_telegram_auth?(auth_data)
     data_check_string = auth_data.sort.map { |k, v| "#{k}=#{v}" }.join("\n")
     secret_key = OpenSSL::Digest::SHA256.new.digest(TELEGRAM_TOKEN)
     hash = OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA256.new, secret_key, data_check_string)
     hash == auth_data['hash']
   end
-
+=end
 def vk_success
     if params[:success]
       session[:authorized] = true

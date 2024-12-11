6a75e788bcab7d5fcd78f8c264474c5db5899672
qrutyy <qrutyq@gmail.com>
2024-12-11 16:55:30 +0300

    fix: im tired

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -6,24 +6,12 @@ class AuthController < ApplicationController
 
   def telegram
     auth_data = params.except(:controller, :action)
-      session[:authorized] = true
-      render json: { message: 'Успешная авторизация', user: auth_data }
-  =begin 
-else
-      session[:authorized] = false
-      render json: { error: 'Неверные данные авторизации' }, status: :unauthorized
-    =end
+    session[:authorized] = true
+    render json: { message: 'Успешная авторизация', user: auth_data }
   end
 
   private
-=begin
-  def valid_telegram_auth?(auth_data)
-    data_check_string = auth_data.sort.map { |k, v| "#{k}=#{v}" }.join("\n")
-    secret_key = OpenSSL::Digest::SHA256.new.digest(TELEGRAM_TOKEN)
-    hash = OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA256.new, secret_key, data_check_string)
-    hash == auth_data['hash']
-  end
-=end
+
   def vk_success
     if params[:success]
       session[:authorized] = true

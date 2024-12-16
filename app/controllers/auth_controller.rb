00b9409f21ec8329032a452366df18c33a308c47
qrutyy <qrutyq@gmail.com>
2024-12-16 10:09:51 +0300

    feat: make tg widget dissapear when authorized
    fix: add debug v10
    
    fix: add debug v11
    
    fix: add debug v12

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -9,11 +9,9 @@ def telegram
     last_name = params[:last_name]
     username = params[:username]
 
-    if first_name != 0 
+    if first_name != "" 
       session[:authorized] = true
       session[:user] = { id: params[:id], first_name: first_name, last_name: last_name, username: username }
-      puts first_name.inspect
-      puts 1111.inspect
       redirect_to root_path, notice: "Успешная авторизация. Добро пожаловать, #{first_name}!"
     else
       session[:authorized] = false
@@ -21,9 +19,8 @@ def telegram
     end
   end
 
-  private
-
   def vk_success
+    puts 2222.inspect
     if params[:success]
       session[:authorized] = true
       render json: { message: 'Session updated successfully' }, status: :ok
